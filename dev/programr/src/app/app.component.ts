814b259c3f519f06fe4616935d8b9e3518634dee
Robin Rehbein <robinrehbein25@gmail.com>
2018-07-26 14:59:55 +0200

    asdf

--- dev/programr/src/app/app.component.ts
+++ dev/programr/src/app/app.component.ts
@@ -6,15 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD
+
   title = 'programr';
   show = false;
   showTitle() {
     this.show = true;
   }
-=======
+
   landing = 'Nice to see you';
   title = 'Welcome to';
   developers = 'Programr';
->>>>>>> 8d777c8acd7d6310fcd4947884a221ce5d787a0b
 }
